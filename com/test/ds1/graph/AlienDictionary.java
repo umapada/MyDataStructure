364f1fff61ef00149b18b67f58a99af4f167aa49
A012573 <umapada.manna@macys.com>
2020-01-22 20:23:55 -0800

    Added new files

---
+++ com/test/ds1/graph/AlienDictionary.java
@@ -0,0 +1,148 @@
+package com.test.ds1.graph;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Given a sorted dictionary of an alien language, find order of characters
+ * Given a sorted dictionary (array of words) of an alien language, find order of characters in the language.
+ * Examples:
+ *
+ * Input:  words[] = {"baa", "abcd", "abca", "cab", "cad"}
+ * Output: Order of characters is 'b', 'd', 'a', 'c'
+ * Note that words are sorted and in the given language "baa"
+ * comes before "abcd", therefore 'b' is before 'a' in output.
+ * Similarly we can find other orders.
+ *
+ * Input:  words[] = {"caa", "aaa", "aab"}
+ * Output: Order of characters is 'c', 'a', 'b'
+ */
+
+/**
+ * The idea is to create a graph of characters and then find topological sorting of the created graph.
+ * Following are the detailed steps.
+ *
+ * 1) Create a graph g with number of vertices equal to the size of alphabet in the given alien language.
+ * For example, if the alphabet size is 5, then there can be 5 characters in words. Initially there are no edges in graph.
+ *
+ * 2) Do following for every pair of adjacent words in given sorted array.
+ * …..a) Let the current pair of words be word1 and word2. One by one compare characters of both words and
+ * find the first mismatching characters.
+ * …..b) Create an edge in g from mismatching character of word1 to that of word2.
+ *
+ * 3) Print topological sorting of the above created graph.
+ *
+ * Following is the implementation of the above algorithm.
+ */
+
+ class AlienDictionary {
+
+    // An array representing the graph as an adjacency list
+    private final List<Integer>[] adjacencyList;
+
+    AlienDictionary(int nVertices)
+    {
+        adjacencyList = new LinkedList[nVertices];
+        for (int vertexIndex = 0; vertexIndex < nVertices; vertexIndex++)
+        {
+            adjacencyList[vertexIndex] = new LinkedList<>();
+        }
+    }
+
+    // function to add an edge to graph
+    void addEdge(int startVertex, int endVertex)
+    {
+        adjacencyList[startVertex].add(endVertex);
+    }
+
+    private int getNoOfVertices()
+    {
+        return adjacencyList.length;
+    }
+
+    // A recursive function used by topologicalSort
+    private void topologicalSortUtil(int currentVertex, boolean[] visited, Stack<Integer> stack)
+    {
+        // Mark the current node as visited.
+        visited[currentVertex] = true;
+        // Recur for all the vertices adjacent to this vertex
+        for (int adjacentVertex : adjacencyList[currentVertex])
+        {
+            if (!visited[adjacentVertex])
+            {
+                topologicalSortUtil(adjacentVertex, visited, stack);
+            }
+        }
+        // Push current vertex to stack which stores result
+        stack.push(currentVertex);
+    }
+
+    // prints a Topological Sort of the complete graph
+    void topologicalSort()
+    {
+        Stack<Integer> stack = new Stack<>();
+        // Mark all the vertices as not visited
+        boolean[] visited = new boolean[getNoOfVertices()];
+        for (int i = 0; i < getNoOfVertices(); i++)
+        {
+            visited[i] = false;
+        }
+        // Call the recursive helper function to store Topological
+        // Sort starting from all vertices one by one
+        for (int i = 0; i < getNoOfVertices(); i++)
+        {
+            if (!visited[i])
+            {
+                topologicalSortUtil(i, visited, stack);
+            }
+        }
+        // Print contents of stack
+        while (!stack.isEmpty())
+        {
+            System.out.print((char)('a' + stack.pop()) + " ");
+        }
+    }
+}
+
+ class OrderOfCharacters
+{
+    // This function fidns and prints order
+    // of characer from a sorted array of words.
+    // alpha is number of possible alphabets
+    // starting from 'a'. For simplicity, this
+    // function is written in a way that only
+    // first 'alpha' characters can be there
+    // in words array. For example if alpha
+    //  is 7, then words[] should contain words
+    // having only 'a', 'b','c' 'd', 'e', 'f', 'g'
+    private static void printOrder(String[] words, int alpha)
+    {
+        // Create a graph with 'aplha' edges
+        AlienDictionary graph = new AlienDictionary(alpha);
+
+        for (int i = 0; i < words.length - 1; i++)
+        {
+            // Take the current two words and find the first mismatching character
+            String word1 = words[i];
+            String word2 = words[i+1];
+            for (int j = 0; j < Math.min(word1.length(), word2.length()); j++)
+            {
+                // If we find a mismatching character, then add an edge from character of word1 to that of word2
+                if (word1.charAt(j) != word2.charAt(j))
+                {
+                    graph.addEdge(word1.charAt(j) - 'a', word2.charAt(j)- 'a');
+                    break;
+                }
+            }
+        }
+        // Print topological sort of the above created graph
+        graph.topologicalSort();
+    }
+    // Driver program to test above functions
+    public static void main(String[] args)
+    {
+        String[] words = {"caa", "aaa", "aab"};
+        printOrder(words, 3);
+    }
+}
---
+++ com/test/ds1/graph/DetectCycleInDirectedGraph.java
@@ -0,0 +1,105 @@
+package com.test.ds1.graph;
+
+/**
+ * Detect Cycle in a Directed Graph
+ * Given a directed graph, check whether the graph contains a cycle or not. Your function should return true if the
+ * given graph contains at least one cycle, else return false.
+ */
+
+/**
+ * Depth First Traversal can be used to detect a cycle in a Graph. DFS for a connected graph produces a tree.
+ * There is a cycle in a graph only if there is a back edge present in the graph. A back edge is an edge that is
+ * from a node to itself (self-loop) or one of its ancestor in the tree produced by DFS.
+ */
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * For a disconnected graph, we get the DFS forest as output. To detect cycle, we can check for a cycle in individual
+ * trees by checking back edges.
+ *
+ * To detect a back edge, we can keep track of vertices currently in recursion stack of function for DFS traversal.
+ * If we reach a vertex that is already in the recursion stack, then there is a cycle in the tree. The edge that
+ * connects current vertex to the vertex in the recursion stack is a back edge. We have used recStack[] array to keep
+ * track of vertices in the recursion stack.
+ */
+
+public class DetectCycleInDirectedGraph {
+
+    private final int V;
+    private final List<List<Integer>> adj;
+
+    public DetectCycleInDirectedGraph(int V)
+    {
+        this.V = V;
+        adj = new ArrayList<>(V);
+
+        for (int i = 0; i < V; i++)
+            adj.add(new LinkedList<>());
+    }
+
+    public static void main(String[] args)
+    {
+        DetectCycleInDirectedGraph graph = new DetectCycleInDirectedGraph(4);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 0);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 3);
+
+        if(graph.isCyclic())
+            System.out.println("Graph contains cycle");
+        else
+            System.out.println("Graph doesn't contain cycle");
+    }
+
+    private boolean isCyclicUtil(int i, boolean[] visited,
+                                 boolean[] recStack)
+    {
+
+        // Mark the current node as visited and part of recursion stack
+        if (recStack[i])
+            return true;
+
+        if (visited[i])
+            return false;
+
+        visited[i] = true;
+
+        recStack[i] = true;
+        List<Integer> children = adj.get(i);
+
+        for (Integer c: children)
+            if (isCyclicUtil(c, visited, recStack))
+                return true;
+
+        recStack[i] = false;
+
+        return false;
+    }
+
+    private void addEdge(int source, int dest) {
+        adj.get(source).add(dest);
+    }
+
+    private boolean isCyclic()
+    {
+
+        // Mark all the vertices as not visited and not part of recursion stack
+        boolean[] visited = new boolean[V];
+        boolean[] recStack = new boolean[V];
+
+
+        // Call the recursive helper function to detect cycle in different DFS trees
+        for (int i = 0; i < V; i++)
+            if (isCyclicUtil(i, visited, recStack))
+                return true;
+
+        return false;
+    }
+
+
+}
